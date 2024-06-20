6b4edf83438e9eed6b4a5c3f99594dae06a5ceba
Gabrielly-silv <163124582+Gabrielly-silv@users.noreply.github.com>
2024-06-20 08:38:22 -0300

    Add files via upload

---
+++ listaObjetos.js
@@ -0,0 +1,17 @@
+const cliente = {
+    nome :"joão",
+    idade: 24,
+    email:"joão@larissa.com",
+    tel: ["(41)9999-0000", "(41)9988-0000"],
+};
+
+cliente.endereco = [
+{
+    rua : "R. Josheph Climber",
+    numero : 1337,
+    apartamento : true,
+    complemento :"Ap 934",
+},
+];
+
+console.log(cliente.endereco);
